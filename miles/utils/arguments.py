a4dc9bb2372f7ccec6940bd910405460ad280c8d
fzyzcjy <ch271828n@outlook.com>
2026-03-20 18:48:07 +0800

    feat: override arguments if dumper enabled
    Add _maybe_apply_dumper_overrides to disable heartbeats, force single
    rollout, and disable eval/save when --dumper-enable is set.
    
    (cherry picked from commit 7e3a76dd4f9121a26ffc93694386385784d9ed3e)

--- miles/utils/arguments.py
+++ miles/utils/arguments.py
@@ -1961,6 +1961,26 @@ def miles_validate_args(args):
             args.use_dynamic_batch_size is False
         ), "Dynamic batch size is not supported for bshd format. Please specify --micro-batch-size instead."
 
+    _maybe_apply_dumper_overrides(args)
+
+
+def _maybe_apply_dumper_overrides(args) -> None:
+    if not args.dumper_enable:
+        return
+
+    if args.use_fault_tolerance:
+        logger.info("Dumper mode: disabling --use-fault-tolerance to suppress RolloutHealthMonitor heartbeats")
+        args.use_fault_tolerance = False
+
+    logger.info("Dumper mode: all heartbeat mechanisms disabled")
+    args.router_disable_health_check = True
+    args.rollout_health_check_interval = 1e18
+
+    logger.info("Dumper mode: forced num_rollout=%d, disabled eval and save", args.num_rollout)
+    args.num_rollout = (args.start_rollout_id or 0) + 1
+    args.eval_interval = None
+    args.save_interval = None
+
 
 def hf_validate_args(args, hf_config):
     def equal(x, y):

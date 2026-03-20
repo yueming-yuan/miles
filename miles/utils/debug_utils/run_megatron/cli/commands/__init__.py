e7cb89aa7955a8e0bd21e76548d74cc69017d2da
fzyzcjy <ch271828n@outlook.com>
2026-03-20 18:56:03 +0800

    feat: add worker main + run command
    Add worker/main.py for standalone Megatron forward/backward via torchrun,
    worker_executor.py for building torchrun commands, run.py CLI command,
    and package entry points (__main__.py, __init__.py). Include tests.
    
    (cherry picked from commit 3aa7f861abe12aee33ed3817d8e5ab92d4a4c052)

---
+++ miles/utils/debug_utils/run_megatron/cli/commands/__init__.py
@@ -0,0 +1,14 @@
+"""CLI commands for run_megatron.
+
+Usage:
+    python -m miles.utils.debug_utils.run_megatron run ...
+    python -m miles.utils.debug_utils.run_megatron show-model-args ...
+"""
+
+import typer
+
+from miles.utils.debug_utils.run_megatron.cli.commands import run
+
+app: typer.Typer = typer.Typer(pretty_exceptions_enable=False)
+
+run.register(app)

7775f1d4af409d0c5b6845a28ebd176674b9bea8
yueming-yuan <yym022502@gmail.com>
2026-05-03 23:05:47 -0700

    fix: tokenizer import path for older megatron-oss base

--- miles/backends/megatron_utils/arguments.py
+++ miles/backends/megatron_utils/arguments.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from megatron.core.tokenizers.utils.build_tokenizer import vocab_size_with_padding as _vocab_size_with_padding
+from megatron.training.tokenizer.tokenizer import _vocab_size_with_padding
 from megatron.training.arguments import parse_args, validate_args
 
 __all__ = ["validate_args", "parse_args", "set_default_megatron_args"]
